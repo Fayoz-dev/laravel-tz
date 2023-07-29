2e2cf9cb7147f31b1e9c7fd9fc6c39e5f7b94c82
Fayoz-dev <frasulov988@gmail.com>
2023-07-29 10:23:59 +0500

    client manager blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    @if (auth()->user()->role->name == 'manager')
+                        You're manager !
+                    @elseif(auth()->user()->role->name == 'client')
+                        You're client !
+                    @endif    
                 </div>
             </div>
         </div>
